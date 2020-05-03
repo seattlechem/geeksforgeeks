0f7a2eef61572ff12a88c35c7da2e2e0dab7cd23
seattlechem <seattlechem@yahoo.com>
2020-05-02 23:30:03 -0700

    linked list: even or odd problem added

---
+++ linked-list-length-even-or-odd/linked_list_even_or_odd.java
@@ -0,0 +1,26 @@
+/*
+class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+*/
+/* isLengthEvenorOdd method should return 1 if the length of LL is even
+   otherwise return 0.*/
+
+class GFG {
+  int isLengthEvenorOdd(Node head1) {
+    Node p = head1;
+
+    while (true) {
+      if (p == null) {
+        return 0;
+      }
+      if (p.next == null) {
+        return 1;
+      }
+      p = p.next.next;
+    }
+  }
+}
