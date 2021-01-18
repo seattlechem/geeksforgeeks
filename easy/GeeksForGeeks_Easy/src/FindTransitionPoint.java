45d6074ad6acfed4662d188197f8c4ed0b5db5a2
seattlechem <seattlechem@yahoo.com>
2021-01-17 16:36:28 -0800

    completion of findTransitionPoint

--- easy/GeeksForGeeks_Easy/src/FindTransitionPoint.java
+++ easy/GeeksForGeeks_Easy/src/FindTransitionPoint.java
@@ -1,5 +1,18 @@
 public class FindTransitionPoint {
     static int transitionPoint(int[] arr, int n) {
-        return 3;
+        int start = 0;
+        int end = n - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == 1) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return (arr[start] == 1 ? start : -1);
+
     }
 }
